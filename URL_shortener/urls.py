ad2180e2bbb9e18cd62fde17114f0399e03369ca
Abhishek Srivastava <=>
2020-08-07 02:00:43 +0530

    Initial Commit

---
+++ URL_shortener/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from URL_shortener import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('shortener.urls')),
+    path('accounts/', include('accounts.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
